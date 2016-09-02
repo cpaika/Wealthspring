869e9f78714f96d94133d20fd79c94f32510bf61
cpaika <cpaika@constantcontact.com>
2016-09-01 22:02:56 -0400

    Adding in associating transactions with accounts

--- app/addTransaction/addTransaction.js
+++ app/addTransaction/addTransaction.js
@@ -9,18 +9,29 @@ angular.module('myApp')
 
 .controller('AddTransactionController', ['$scope', '$location', function($scope, $location) {
 
-    $scope.addTransaction = function(payee, account, category, amount, cleared) {
+    $scope.addTransaction = function(payee, accountName, category, amount, cleared) {
+        var _this = this;
         firebase.auth().onAuthStateChanged(function(user) {
             if (user) {
-                firebase.database().ref('users/' + user.uid).push({
-                    payee: payee,
-                    account: account,
-                    category: category,
-                    amount: amount,
-                    cleared: false
+                //Only listen once otherwise never ending loop...
+                firebase.database().ref('users/' + user.uid + '/accounts').once('value').then(function(snapshot) {
+                    _this.accounts = snapshot.val();
+                    for(var account in _this.accounts) {
+                        if(_this.accounts[account].name === accountName) {
+                            _this.accountName = account
+                            break;
+                        }
+                    }
+                    firebase.database().ref('users/' + user.uid + '/accounts/' + _this.accountName).push({
+                        payee: payee,
+                        account: account,
+                        category: category,
+                        amount: amount,
+                        cleared: false
+                    }); 
                 });
             } else {
-                // No user is signed in.
+            // No user is signed in.
             }
         });
     };
